15c43cc04e9148870c807c5c2f03e99840444627
gzq <517766570@qq.com>
2020-12-16 20:39:43 +0800

    项目初始化

---
+++ birthday/birthday/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  birthday
+//
+//  Created by atoz on 2020/12/16.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ birthday/birthday/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  birthday
+//
+//  Created by atoz on 2020/12/16.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
